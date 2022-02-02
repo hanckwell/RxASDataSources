5ea2154b0156dc9ae9222ac17f480fc78a2dd2ef
Александр Клёмин <hanckwell@gmail.com>
2022-02-02 15:42:04 +0300

    Package.swift added

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RxASDataSources",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "RxASDataSources",
+            targets: ["RxASDataSources"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/while366/Texture.git", .branch("spm")),
+        .package(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", .upToNextMajor(from: "5.0.1")),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "RxASDataSources",
+            dependencies: [
+                "RxSwift",
+                .product(name: "RxCocoa", package: "RxSwift"),
+                .product(name: "Differentiator", package: "RxDataSources"),
+                .product(name: "RxDataSources", package: "RxDataSources"),
+                .product(name: "AsyncDisplayKit", package: "Texture"),
+            ],
+            path: "Sources/DataSources"),
+
+    ]
+)
